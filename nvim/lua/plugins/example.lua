ce33a98802d905550cb96440e7fe3216c6668fbc
Yiming Peng <yimingpengjojo@gmail.com>
2025-05-04 14:57:30 +1200

    Upgrade lazyvim and fixed some issues there and some cleanups

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -9,16 +9,6 @@ if false then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
 
   -- change trouble config
   {
@@ -39,58 +29,6 @@ return {
       table.insert(opts.sources, { name = "emoji" })
     end,
   },
-
-  -- add pyright to lspconfig
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- pyright will be automatically installed with mason and loaded with lspconfig
-  --       pyright = {},
-  --     },
-  --   },
-  -- },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   dependencies = {
-  --     "jose-elias-alvarez/typescript.nvim",
-  --     init = function()
-  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
-  --         -- stylua: ignore
-  --         vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-  --       end)
-  --     end,
-  --   },
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- tsserver will be automatically installed with mason and loaded with lspconfig
-  --       tsserver = {},
-  --     },
-  --     -- you can do any additional lsp server setup here
-  --     -- return true if you don't want this server to be setup with lspconfig
-  --     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-  --     setup = {
-  --       -- example to setup with typescript.nvim
-  --       tsserver = function(_, opts)
-  --         require("typescript").setup({ server = opts })
-  --         return true
-  --       end,
-  --       -- Specify * to use this function as a fallback for any server
-  --       -- ["*"] = function(server, opts) end,
-  --     },
-  --   },
-  -- },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  -- { import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- add more treesitter parsers
   {
@@ -155,20 +93,4 @@ return {
 
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  -- { import = "lazyvim.plugins.extras.lang.json" },
-
-  -- add any tools you want to have installed below
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "stylua",
-  --       "shellcheck",
-  --       "shfmt",
-  --       "flake8",
-  --     },
-  --   },
-  -- },
 }
