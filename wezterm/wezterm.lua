f0223c6e8fed12ff04118634fdfb9945c8e4e3e2
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-09 23:01:09 +1200

    Update wezterm to do some tweaking

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,14 +6,14 @@ local config = wezterm.config_builder()
 -- Set your configuration options
 config.automatically_reload_config = true
 config.enable_tab_bar = true
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.show_tabs_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE" -- disable the title bar but enable the resisable border
 config.default_cursor_style = "BlinkingBar"
-config.color_scheme = "rose-pine-moon"
--- config.color_scheme = "Catppuccin Mocha (Gogh)"
+-- config.color_scheme = "rose-pine-moon"
+config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.font = wezterm.font("Hack Nerd Font", { weight = "Bold" })
 config.font_size = 18
 config.background = {
@@ -38,12 +38,12 @@ config.background = {
 		opacity = 0.96,
 	},
 }
-config.window_padding = {
-	left = 1,
-	right = 1,
-	top = 0,
-	bottom = 0,
-}
+-- config.window_padding = {
+-- 	left = 1,
+-- 	right = 1,
+-- 	top = 0,
+-- 	bottom = 0,
+-- }
 
 -- Add key bindings
 config.keys = {
@@ -56,11 +56,11 @@ config.keys = {
 }
 
 -- Control the screen size
-local mux = wezterm.mux
-wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
-	window:gui_window():maximize()
-end)
+-- local mux = wezterm.mux
+-- wezterm.on("gui-startup", function()
+-- 	local tab, pane, window = mux.spawn_window({})
+-- 	window:gui_window():maximize()
+-- end)
 
 -- control dim
 -- Dim unfocused windows so the focused one is obvious at a glance.
