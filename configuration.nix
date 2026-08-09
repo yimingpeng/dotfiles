0e4034906d82dcfe102a493f1aa5d86fd785af83
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-09 13:33:18 +1200

    Fix the true and false

--- configuration.nix
+++ configuration.nix
@@ -2,9 +2,9 @@
 
 {
   # Leave the determinate to handle the Nix, so here we disable it from nix-darwin 
-  nix.enable = False;
+  nix.enable = false;
 
-  nixpkgs.config.allowUnfree = True; # allow us to install apps that are not completely free
+  nixpkgs.config.allowUnfree = true; # allow us to install apps that are not completely free
   nixpkgs.hostPlatform = "aarch64-darwin"; # use x86-64-darwin for Intel CPU
   
   # Define the main user, get from `id` command
