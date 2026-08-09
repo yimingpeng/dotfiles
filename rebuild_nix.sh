22267a021017f888a580a42e95244d82e6e9dde4
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-09 15:32:13 +1200

    Add a rebuild_nix.sh

---
+++ rebuild_nix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+if [ "$DIR" != "$HOME/.dotfiles" ]; then
+  ln -sfn "$DIR" "$HOME/.dotfiles"
+fi
+ln -sfn "$DIR" ~/.dotfiles
+exec sudo darwin-rebuild switch --flake ~/.dotfiles/nix#Yiming-macbook-m1-pro
