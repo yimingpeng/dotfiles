cbdeb59d88cc894fd92de14f740cbc0b46c75d76
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-09 13:26:16 +1200

    Get the nix setup for my mac

---
+++ configuration.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  # Leave the determinate to handle the Nix, so here we disable it from nix-darwin 
+  nix.enable = False;
+
+  nixpkgs.config.allowUnfree = True; # allow us to install apps that are not completely free
+  nixpkgs.hostPlatform = "aarch64-darwin"; # use x86-64-darwin for Intel CPU
+  
+  # Define the main user, get from `id` command
+  system.primaryUser = 'yimingpeng';
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Yiming's darwin system";
+
+  inputs = {
+    # Use `github:NixOS/nixpkgs/nixpkgs-26.05-darwin` to use Nixpkgs 26.05.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    # Use `github:nix-darwin/nix-darwin/nix-darwin-26.05` to use Nixpkgs 26.05.
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
+    darwinConfigurations."Yiming-macbook-m1-pro" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
