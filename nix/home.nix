97493ff325b95ed8ddcc91a184ff82b6d4392d54
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-11 19:56:43 +1200

    add npm

--- nix/home.nix
+++ nix/home.nix
@@ -21,6 +21,8 @@ in
   ];
   fonts.fontconfig.enable = true;
   home.sessionVariables.EDITOR = "nvim";
+  home.sessionVariables.NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/.npm-global";
+  home.sessionPath = [ "${config.home.homeDirectory}/.npm-global/bin" ];
   
   # config the zsh
   programs.zsh = {
