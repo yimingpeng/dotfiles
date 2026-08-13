9e4aa63ea31ac6e7aeb4260ad0a6b22b2aa73d5d
Yiming Peng <yimingpengjojo@gmail.com>
2026-08-14 08:53:37 +1200

    Remove ob from imac and update rebuild

--- rebuild_nix.sh
+++ rebuild_nix.sh
@@ -5,4 +5,4 @@ if [ "$DIR" != "$HOME/.dotfiles" ]; then
   ln -sfn "$DIR" "$HOME/.dotfiles"
 fi
 ln -sfn "$DIR" ~/.dotfiles
-exec sudo darwin-rebuild switch --flake ~/.dotfiles/nix#Yiming-macbook-m1-pro
+exec sudo darwin-rebuild switch --flake ~/.dotfiles/nix#Yiming-iMac
